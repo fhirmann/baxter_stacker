aec1730181039220438c096c929748a9902509b6
Hugo <hugo.leurent@student.tugraz.at>
2019-12-10 14:10:33 +0100

    Parser and service + Readme update
    Former-commit-id: 8bf71f22bacf47835e7fad3c38f0b421d7a02a3c

--- speech_recognition/README.md
+++ speech_recognition/README.md
@@ -6,7 +6,7 @@ To make the google speech API work  you will need to possess the secret access k
 You will need to set up the path in the .bashrc by writing :
 `export GOOGLE_APPLICATION_CREDENTIALS=$HOME/catkin_ws/src/kmr19/speech_recognition/gcsapi_user_credentials.json`
 
-## Set up the environment ##
+## Set up the Google Speech environment ##
 
 We base our program on the Google Speech API and some other python libraries. To make it easier and not naving to install it everytime, everything that's needed is contained in a python environment (folder env). To make the package work, we have to work in this virtual Python environment :
 Go to the speech_recognition folder and execute the command : `source env/bin/activate`
@@ -15,13 +15,32 @@ To leave the environment, just do `deactivate`
 Nodes should be already set as executables. If not, go in the nodes folder and do :
 `chmod +x speech_request_node.py` and `chmod +x parser_client_node.py`
 
+## Set up the Stanford Parser environment ##
+
+To make the Stanford parser work, we need to download the server files, install Java and run the server in the background to parse the data.
+Download the archive at https://stanfordnlp.github.io/CoreNLP/download.html and extract it where you want (I would personnaly put it in the speech reco package to have everything together)
+
+Install the Java jre and jdk to make it work properly : 
+`sudo apt-get update`
+`sudo apt-get install default-jre`
+`sudo apt-get install default-jdk`
+
+You will also need some libraries such as nltk. After activating your virtual env, do :
+`pip3 install -U nltk`
+
+In a separate terminal, go inside the StanfordNLP folder and launch the NLP server in the background with the command :
+`java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000`
+
 ## How it works ##
 
 Launch the commands `roscore`, `rosrun speech_recognition speech_request_node.py` and `rosrun speech_recognition parser_client_node.py` in 3 different terminals. Don't forget the `source devel/setup.bash` from the catkin workspace if you don't see the nodes.
 
+Remark : The speech recognition node will close itself after 305 seconds.
+
 When the user will say "hello Baxter", the speech recog node will print "Listening...". This means that the next request will be sent to the other node, the subscriber. You always have to say "hello Baxter" and to wait for the "Listening..." message before sending a request.
+
+The request will be parsed and set up with correct semantics to be sent to the reasoning and planning part.
 
 ## TODO ##
 
-Use the stanford parser to parse correctly the information from the request. Once this is done, return a correct request based on the semantics set up in StackGoalService.srv and send it to the reasoning and planning part.
-
+Install everything on the master PC and check with the others if it's correctly integrated in the pipeline
--- speech_recognition/nodes/parser_client_node.py
+++ speech_recognition/nodes/parser_client_node.py
@@ -2,20 +2,120 @@
 import rospy
 
 #from reasoning_planning_helper.reasoning_planning_interface import *
+from nltk.parse import CoreNLPParser
+import nltk
 
 from reasoning_planning.srv import StackGoalService, StackGoalServiceResponse, StackGoalServiceRequest
 
 from std_msgs.msg import String
 
+parser = CoreNLPParser(url='http://localhost:9000')
 
+action = []
+top_block = []
+below_block = []
+position = []
+
+#Assign a semantic value to the request req depending on the string s
+def assignServiceValue(req, s, which_block):
+    
+    int_value = -1
+    if (s == "red" or s == "Red"):
+        int_value = req.RED
+    elif (s == "blue" or s == "Blue"):
+        int_value = req.BLUE
+    elif (s == "yellow" or s == "Yellow"):
+        int_value = req.YELLOW
+    elif (s == "green" or s == "Green"):
+        int_value = req.GREEN
+    elif (s == "left"):
+        int_value = req.LEFT_RELATIVE
+    elif (s == "right"):
+        int_value = req.RIGHT_RELATIVE
+    elif (s == "far"):
+        int_value = req.FAR_RELATIVE
+    elif (s == "close"):
+        int_value = req.CLOSE_RELATIVE
+    elif (s == "higher"):
+        int_value = req.HIGHER_RELATIVE
+    elif (s == "lower"):
+        int_value = req.LOWER_RELATIVE
+        
+    if (int_value != -1):
+        if (which_block == 0):
+            req.top_block_semantics.append(int_value)
+        elif (which_block == 1):
+            req.below_block_semantics.append(int_value)
+        
+    return req
+    
+#Use a recursive function to separate the elements from the parsed tree into exploitable data (4 lists containing the action, the position and the info for each block)  
+def getNodes(parent):
+    ROOT = 'ROOT'
+    global action, top_block, below_block, position
+    for node in parent:
+        if type(node) is nltk.Tree:
+            if node.label() != ROOT:
+                if (node.label() == 'VB'):
+                    action = node.leaves()
+                elif (node.label() == 'IN'):
+                    position = node.leaves()
+                elif (node.label() == 'NP'):
+                    if (len(top_block) == 0):
+                        top_block = node.leaves()
+                    else : 
+                        below_block = node.leaves()
+            getNodes(node)
+
+#Calls the getnode function and creates a list containing the 4 previous lists
+def getRequestList(tree):
+    global action, top_block, below_block, position
+    getNodes(tree)    
+    final_list = [action, top_block, position, below_block]
+    return(final_list)
+
+#Sends the finalized request to the service
+def sendToService(req):
+    rospy.init_node('reasoning_planning_service_client_test_node')
+
+    rospy.loginfo("Waiting for service /stack_goal_service...")
+    rospy.wait_for_service('/stack_goal_service')
+
+    rospy.loginfo("Service /stack_goal_service is online!")
+    stack_goal_service = rospy.ServiceProxy('/stack_goal_service', StackGoalService)
+
+    req = create_simple_test_scene_request_correct()
+
+    rospy.loginfo("request:")
+    print (req)
+
+    response = stack_goal_service(req)
+
+    rospy.loginfo("response:")
+    print (response)
+ 
+#Callback function from the listener. Applies the Stanford Parser and creates the request
 def parse_create_scene(data):
+    global action, top_block, position, below_block
+    action = []
+    top_block = []
+    below_block = []
+    position = []
+    
     rospy.loginfo(rospy.get_caller_id() + "I heard : %s", data.data)
-
-    #TODO
-    #PARSE WITH STANFORD PARSER
-    #Result will be 2 structs containing infos such as location, color
-    #we will put those structs in a fction that will return the request with correct semantics
-    #Then send it to the server as in the example
+    request_parsed = list(parser.parse(data.data.split()))
+    request_list = getRequestList(request_parsed)
+    print(request_list)
+    
+    req = StackGoalServiceRequest()
+    for i in range (len(request_list[1])): 
+        req = assignServiceValue(req, request_list[1][i], 0)
+    for i in range (len(request_list[3])): 
+        req = assignServiceValue(req, request_list[3][i], 1)
+    print(req.top_block_semantics)
+    print(req.below_block_semantics)
+    
+    sendToService(req)
 
 def listener():
 
@@ -32,4 +132,5 @@ def listener():
     rospy.spin()
 
 if __name__ == '__main__':
+    
     listener()
