7c506fa8270596793d94828c76422e7faef99e47
baxter <baxter@baxter.com>
2020-01-07 16:11:23 +0100

    aligned camera with world frame
    Former-commit-id: 405ff04036480d6b426819a6bfc173bae0820df2

--- perception/nodes/tf_camera_link_broadcaster.py
+++ perception/nodes/tf_camera_link_broadcaster.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python  
 import rospy
 import tf
+from math import pi
 
 if __name__ == '__main__':
     rospy.init_node('tf_camera_link_broadcaster')
@@ -9,8 +10,8 @@ if __name__ == '__main__':
     first = 0
 
     while not rospy.is_shutdown():
-        br.sendTransform((0.14, 0.1, 0.835),
-                         tf.transformations.quaternion_from_euler(0.0, 0.9, 0.0),
+        br.sendTransform((0.175, 0.075, 0.835),
+                         tf.transformations.quaternion_from_euler(0.0, 0.9, 0.05),
                          rospy.Time.now(),
                          "camera_link",
                          "world")
