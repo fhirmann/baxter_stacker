b6a69a9b202c7fe2fd6fbf96623fc149a4afab08
baxter <baxter@baxter.com>
2020-01-14 15:46:13 +0100

    better path
    Former-commit-id: b3560187d5427f3174f9845e8aae7898484df860

--- speech_recognition/scripts/launch_stanford_server.py
+++ speech_recognition/scripts/launch_stanford_server.py
@@ -8,5 +8,5 @@ import rospy
 '''os.system('roscd speech_recognition/stanford-corenlp-full-2018-10-05 && java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000')'''
 
 
-p = subprocess.Popen(["java", "-mx4g", '-cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer', "-port 9000", "-timeout 15000"], cwd="~/catkin_ws/src/kmr19/speech_recognition/stanford-corenlp-full-2018-10-05")
+p = subprocess.Popen(["java", "-mx4g", '-cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer', "-port 9000", "-timeout 15000"], cwd="/home/baxter/catkin_ws/src/kmr19/speech_recognition/stanford-corenlp-full-2018-10-05")
 p.wait()
